121e5f35ab11c631290124300db1daff9d5b9b30
Pardis Zahraei <paradisez2001@gmail.com>
2023-12-08 12:31:16 +0330

    DateExtractor changed

--- DateExtractor.py
+++ DateExtractor.py
@@ -20,6 +20,14 @@ def detect_words(text, position, week, holiday, month, numbers):
                     else:
                         return next_word
                 elif next_word in numbers and i < len(text) - 1:  # check the word after the number
+                    if((next_word=="بیست" or next_word=="سی") and text[i+2]=="و"):
+                        next2_word = text[i + 2]
+                        next3_word = text[i + 3]
+                        next4_word = text[i + 4]
+                        if next4_word in month:
+                            return next_word + ' ' + next2_word + ' ' + next3_word + ' '+next4_word
+                        else:
+                            return next_word + ' ' + next2_word + ' ' + next3_word
                     next2_word = text[i+2]
                     next3_word = text[i + 3]
                     try:
@@ -57,17 +65,24 @@ def detect_words(text, position, week, holiday, month, numbers):
                         return prev_word
                 elif prev_word in month and i > 1:  # check the word after the number
                     prev2_word = text[i-2]
+                    prev3_word = text[i - 3]
+                    prev4_word=text[i-4]
+                    if (prev3_word=="و" and (prev4_word=="بیست" or prev4_word=="سی")):
+                        if prev2_word in numbers:
+                            return prev4_word+' '+prev3_word+' '+prev2_word + ' ' + prev_word
+                        else:
+                            return prev_word
                     if prev2_word in numbers:
                         return prev2_word + ' ' + prev_word
                     else:
                         return prev_word
-    return prev_word
+    return ""
 week = ['شنبه', 'یکشنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنجشنبه', 'جمعه']
 month = ['مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند', 'فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور']
 spec_numbers = ['یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه', 'ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده', 'بیست', 'بیست و یک', 'بیست و دو', 'بیست و سه', 'بیست و چهار', 'بیست و پنج', 'بیست و شش', 'بیست و هفت', 'بیست و هشت', 'بیست و نه', '1','2','3','4','5','6','7','8','9','10','سی','11','13','14','15','16','17','18','19','12','20','23','24','25','26','27','28','29','30','21','22','31',]
 holidays=['عید','تابستون','زمستون','کریسمس','بهار','پاییز','امروز','فردا','محرم']
 pre=['از','شروع']
-pos=['تا','پایان']
+pos=['تا','تمام','تموم','پایان']
 ordinal_numbers = ["یکم", "دوم", "سوم", "چهارم", "پنجم", "ششم", "هفتم", "هشتم", "نهم", "دهم",
                    "یازدهم", "دوازدهم", "سیزدهم", "چهاردهم", "پانزدهم", "شانزدهم", "هفدهم", "هجدهم", "نوزدهم", "بیستم",
                    "بیست و یکم", "بیست و دوم", "بیست و سوم", "بیست و چهارم", "بیست و پنجم", "بیست و ششم", "بیست و هفتم", "بیست و هشتم", "بیست و نهم", "سی‌ام", "سی و یکم"]
@@ -75,7 +90,7 @@ farsi_numbers = ["۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹", "۱۰", "
 
 concatenated_list =ordinal_numbers+spec_numbers+farsi_numbers
 
-text="این تسک را از 5 شهریور تا کریسمس وقت دارید که حل کنید."
+text="لطفا این کار رو در سی و یکم خرداد شروع کنید و حتما تا پاییز کار تموم باشه دیگع."
 
 print(detect_words(text, pre, week, holidays, month, concatenated_list))  # Output: ['15 July']
 print(detect_words(text, pos, week, holidays, month, concatenated_list))  # Output: ['15 July']

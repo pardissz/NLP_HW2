584f3096b2524102d79d49ce2065f71b2453088b
Mahdi Saadatbakht <mahdi.saadatbakht.1381@gmail.com>
2023-12-10 08:36:33 +0330

    Make methods not static in StatusChecker.py

--- StatusChecker.py
+++ StatusChecker.py
@@ -1,27 +1,29 @@
 class StatusChecker:
-    finish_status = ['تموم شد', 'اتمام یافت', 'تمام شد', 'تمام است', 'پایان یافت', 'تمومه', 'تمام شده', 'اوکی شد',
-                     'اوکی شده', 'تموم شده', 'به اتمام رسید']
-    urgency_status = ['حتما', 'فورا', 'سریعا', 'به سرعت', 'به شدت', 'حتماً', 'فوراً', 'سریعاً', 'قطعاً']
+    def __init__(self):
+        self.finish_status = ['تموم شد', 'اتمام یافت', 'تمام شد', 'تمام است', 'پایان یافت', 'تمومه', 'تمام شده',
+                              'اوکی شد',
+                              'اوکی شده', 'تموم شده', 'به اتمام رسید']
+        self.urgency_status = ['حتما', 'فورا', 'سریعا', 'به سرعت', 'به شدت', 'حتماً', 'فوراً', 'سریعاً', 'قطعاً']
 
-    @staticmethod
-    def check_status(text):
+    def check_status(self, text):
         is_done = False
         is_urgent = False
         text_words = text.split()
-        for bigram in StatusChecker.finish_status:
+        for bigram in self.finish_status:
             if bigram in text:
                 is_done = True
                 break
-        if any(word in text_words for word in StatusChecker.urgency_status):
+        if any(word in text_words for word in self.urgency_status):
             is_urgent = True
         return is_done, is_urgent
 
 
 if __name__ == '__main__':
+    status_checker = StatusChecker()
     text = 'من حتما  میگم که تموم شده '
-    is_done, is_urgent = StatusChecker.check_status(text)
+    is_done, is_urgent = status_checker.check_status(text)
     print('is_done:', is_done, ', is_urgent:', is_urgent)
 
     text = 'تسک به اتمام رسید'
-    is_done, is_urgent = StatusChecker.check_status(text)
+    is_done, is_urgent = status_checker.check_status(text)
     print('is_done:', is_done, ', is_urgent:', is_urgent)
